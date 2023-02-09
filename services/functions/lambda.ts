92b347615c5f419bd1bbc14de1747ad94b5c547d
William <your@email.here>
2023-02-08 19:24:14 -0800

    delete some commented code

--- services/functions/lambda.ts
+++ services/functions/lambda.ts
@@ -53,46 +53,12 @@ const appRouter = t.router({
 })
 
 
-
-
-
-// greeting: t.procedure
-// .input(
-//     z.object({
-//       name:  z.string(),
-//       })
-//   )
-//   .query((req) => {
-//     req.input
-//   return { 
-//       text: `Hello ${req.input?.name ?? 'world'}`
-//   };
-// }),
 export type AppRouter = typeof appRouter;
 
 export const handler = awsLambdaRequestHandler({
   router: appRouter,
 })
-// greetings: t.procedure
-// .input(
-  //   z.object({
-    //     names: z.array(z.string()),
-    //   })
-    // )
-    // .query(({input}) => {
-      //   return {
-        //     name:
-        //   }
-        // const createContext = ({
-        //   event,
-        //   context,
-        // }: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({
-        //   headers: {
-        //     'Access-Control-Allow-Origin': 'http://localhost:3000',
-        //     'Access-Control-Allow-Credentials': true,
-        //   },
-        // }) // no context
-// })
+
 
 
 
